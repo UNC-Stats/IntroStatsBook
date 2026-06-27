cede137404b73998078c5c266d124dffc59236e0
Kristin Kang <kristin.kang@unco.edu>
2026-06-27 23:03:44 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,491 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "section-ch1-greetings",
+  "level": "1",
+  "url": "section-ch1-greetings.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Greetings",
+  "body": " Greetings  Text of section.  "
+},
+{
+  "id": "section-ch1-learning-stats",
+  "level": "1",
+  "url": "section-ch1-learning-stats.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "How to Learn and Think about Statistics",
+  "body": " How to Learn and Think about Statistics  Text of section.  "
+},
+{
+  "id": "section-ch1-tips",
+  "level": "1",
+  "url": "section-ch1-tips.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Tips for Success",
+  "body": " Tips for Success  Text of section.  "
+},
+{
+  "id": "section-ch2-stat-invest-process",
+  "level": "1",
+  "url": "section-ch2-stat-invest-process.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "A Full Investigation",
+  "body": " A Full Investigation  Text of section.  "
+},
+{
+  "id": "section-ch2-big-ideas-vocab",
+  "level": "1",
+  "url": "section-ch2-big-ideas-vocab.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Big Idea and Vocabulary",
+  "body": " Big Idea and Vocabulary  Text of section.  "
+},
+{
+  "id": "section-ch2-RQ",
+  "level": "1",
+  "url": "section-ch2-RQ.html",
+  "type": "Section",
+  "number": "2.3",
+  "title": "Good Research Questions",
+  "body": " Good Research Questions  Text of section.  "
+},
+{
+  "id": "section-ch3-spotify",
+  "level": "1",
+  "url": "section-ch3-spotify.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Large Spotify Data Set",
+  "body": " Large Spotify Data Set  Text of section.  "
+},
+{
+  "id": "section-ch3-multivar-thinking",
+  "level": "1",
+  "url": "section-ch3-multivar-thinking.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Multivariable Thinking",
+  "body": " Multivariable Thinking  Text of section.  "
+},
+{
+  "id": "section-ch3-other-data-viz",
+  "level": "1",
+  "url": "section-ch3-other-data-viz.html",
+  "type": "Section",
+  "number": "3.3",
+  "title": "More Data Visualization Options",
+  "body": " More Data Visualization Options  Text of section.  "
+},
+{
+  "id": "section-ch3-interrogate-data",
+  "level": "1",
+  "url": "section-ch3-interrogate-data.html",
+  "type": "Section",
+  "number": "3.4",
+  "title": "Interrogating Data",
+  "body": " Interrogating Data  Text of section.  "
+},
+{
+  "id": "section-ch4-simulation-intro",
+  "level": "1",
+  "url": "section-ch4-simulation-intro.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Introduction to Modeling and Simulation",
+  "body": " Introduction to Modeling and Simulation  Text of section.  "
+},
+{
+  "id": "section-ch4-summarize-sim",
+  "level": "1",
+  "url": "section-ch4-summarize-sim.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "Making Sense of Simulation Results",
+  "body": " Making Sense of Simulation Results  Text of section.  "
+},
+{
+  "id": "section-ch4-example-sim",
+  "level": "1",
+  "url": "section-ch4-example-sim.html",
+  "type": "Section",
+  "number": "4.3",
+  "title": "Another Simulation Study",
+  "body": " Another Simulation Study  Text of section.  "
+},
+{
+  "id": "section-ch5-summarize-one-cat",
+  "level": "1",
+  "url": "section-ch5-summarize-one-cat.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Summarizing one Categorical Variable",
+  "body": " Summarizing one Categorical Variable  Text of section.  "
+},
+{
+  "id": "section-ch5-inference-intro-HT",
+  "level": "1",
+  "url": "section-ch5-inference-intro-HT.html",
+  "type": "Section",
+  "number": "5.2",
+  "title": "Hypothesis Testing",
+  "body": " Hypothesis Testing  Text of section.  "
+},
+{
+  "id": "section-ch5-null-model-sim",
+  "level": "1",
+  "url": "section-ch5-null-model-sim.html",
+  "type": "Section",
+  "number": "5.3",
+  "title": "Model and Simulate Null Hypotheses",
+  "body": " Model and Simulate Null Hypotheses  Text of section.  "
+},
+{
+  "id": "section-ch6-quantify-evidence",
+  "level": "1",
+  "url": "section-ch6-quantify-evidence.html",
+  "type": "Section",
+  "number": "6.1",
+  "title": "Quantifying Evidence Against the Null Hypothesis",
+  "body": " Quantifying Evidence Against the Null Hypothesis  Text of section.  "
+},
+{
+  "id": "section-ch6-decision-errors",
+  "level": "1",
+  "url": "section-ch6-decision-errors.html",
+  "type": "Section",
+  "number": "6.2",
+  "title": "Understanding Errors",
+  "body": " Understanding Errors  Text of section.  "
+},
+{
+  "id": "section-ch7-generalize",
+  "level": "1",
+  "url": "section-ch7-generalize.html",
+  "type": "Section",
+  "number": "7.1",
+  "title": "Considerations for Generalization",
+  "body": " Considerations for Generalization  Text of section.  "
+},
+{
+  "id": "section-ch7-sampling-method",
+  "level": "1",
+  "url": "section-ch7-sampling-method.html",
+  "type": "Section",
+  "number": "7.2",
+  "title": "Sampling Methods",
+  "body": " Sampling Methods  Text of section.  "
+},
+{
+  "id": "section-ch8-sampling-dist",
+  "level": "1",
+  "url": "section-ch8-sampling-dist.html",
+  "type": "Section",
+  "number": "8.1",
+  "title": "Sampling Distributions",
+  "body": " Sampling Distributions  Text of section.  "
+},
+{
+  "id": "section-ch8-bootstrap",
+  "level": "1",
+  "url": "section-ch8-bootstrap.html",
+  "type": "Section",
+  "number": "8.2",
+  "title": "Bootstrapping to Estimate Standard Error",
+  "body": " Bootstrapping to Estimate Standard Error  Text of section.  "
+},
+{
+  "id": "section-ch8-estimation-intro",
+  "level": "1",
+  "url": "section-ch8-estimation-intro.html",
+  "type": "Section",
+  "number": "8.3",
+  "title": "Parameter Estimation",
+  "body": " Parameter Estimation  Text of section.  "
+},
+{
+  "id": "section-ch9-construct-ci-1m1p",
+  "level": "1",
+  "url": "section-ch9-construct-ci-1m1p.html",
+  "type": "Section",
+  "number": "9.1",
+  "title": "Construction",
+  "body": " Construction  Text of section.  "
+},
+{
+  "id": "section-ch9-interpret-ci",
+  "level": "1",
+  "url": "section-ch9-interpret-ci.html",
+  "type": "Section",
+  "number": "9.2",
+  "title": "Interpretation",
+  "body": " Interpretation  Text of section.  "
+},
+{
+  "id": "section-ch9-test-by-ci",
+  "level": "1",
+  "url": "section-ch9-test-by-ci.html",
+  "type": "Section",
+  "number": "9.3",
+  "title": "Relationship with Hypothesis Testing",
+  "body": " Relationship with Hypothesis Testing  Text of section.  "
+},
+{
+  "id": "section-ch10-summarize-one-quant",
+  "level": "1",
+  "url": "section-ch10-summarize-one-quant.html",
+  "type": "Section",
+  "number": "10.1",
+  "title": "Summarizing one Quantitative Variable",
+  "body": " Summarizing one Quantitative Variable  Text of section.  "
+},
+{
+  "id": "section-ch10-ht-bootstrap-sim",
+  "level": "1",
+  "url": "section-ch10-ht-bootstrap-sim.html",
+  "type": "Section",
+  "number": "10.2",
+  "title": "Model and Simulate with the Bootstrap",
+  "body": " Model and Simulate with the Bootstrap  Text of section.  "
+},
+{
+  "id": "section-ch10-ht-bootstrap-evaluate",
+  "level": "1",
+  "url": "section-ch10-ht-bootstrap-evaluate.html",
+  "type": "Section",
+  "number": "10.3",
+  "title": "Evaluate",
+  "body": " Evaluate  Text of section.  "
+},
+{
+  "id": "section-ch11-theory-intro",
+  "level": "1",
+  "url": "section-ch11-theory-intro.html",
+  "type": "Section",
+  "number": "11.1",
+  "title": "Theoretical Modeling with Probability Distributions",
+  "body": " Theoretical Modeling with Probability Distributions  Text of section.  "
+},
+{
+  "id": "section-ch11-CLT-one-prop",
+  "level": "1",
+  "url": "section-ch11-CLT-one-prop.html",
+  "type": "Section",
+  "number": "11.2",
+  "title": "Inference for One Proportion",
+  "body": " Inference for One Proportion  Text of section.  "
+},
+{
+  "id": "section-ch11-CLT-one-mean",
+  "level": "1",
+  "url": "section-ch11-CLT-one-mean.html",
+  "type": "Section",
+  "number": "11.3",
+  "title": "Inference for One Mean",
+  "body": " Inference for One Mean  Text of section.  "
+},
+{
+  "id": "section-ch12-summarize-quant-and-cat",
+  "level": "1",
+  "url": "section-ch12-summarize-quant-and-cat.html",
+  "type": "Section",
+  "number": "12.1",
+  "title": "Summarizing Two Variables",
+  "body": " Summarizing Two Variables  Text of section.  "
+},
+{
+  "id": "section-ch12-ht-randomization-2m",
+  "level": "1",
+  "url": "section-ch12-ht-randomization-2m.html",
+  "type": "Section",
+  "number": "12.2",
+  "title": "Randomization Test to Compare Two Means",
+  "body": " Randomization Test to Compare Two Means  Text of section.  "
+},
+{
+  "id": "section-ch12-anova",
+  "level": "1",
+  "url": "section-ch12-anova.html",
+  "type": "Section",
+  "number": "12.3",
+  "title": "ANOVA for Comparing Multiple Means",
+  "body": " ANOVA for Comparing Multiple Means  Text of section.  "
+},
+{
+  "id": "section-ch12-study-design",
+  "level": "1",
+  "url": "section-ch12-study-design.html",
+  "type": "Section",
+  "number": "12.4",
+  "title": "Study Design and Causal Conclusions",
+  "body": " Study Design and Causal Conclusions  Text of section.  "
+},
+{
+  "id": "section-ch12-paired-data",
+  "level": "1",
+  "url": "section-ch12-paired-data.html",
+  "type": "Section",
+  "number": "12.5",
+  "title": "Special Case - Paired Data",
+  "body": " Special Case - Paired Data  Text of section.  "
+},
+{
+  "id": "section-ch13-summarize-two-cat",
+  "level": "1",
+  "url": "section-ch13-summarize-two-cat.html",
+  "type": "Section",
+  "number": "13.1",
+  "title": "Summarizing Two Categorical Variables",
+  "body": " Summarizing Two Categorical Variables  Text of section.  "
+},
+{
+  "id": "section-ch13-ht-randomization-2p",
+  "level": "1",
+  "url": "section-ch13-ht-randomization-2p.html",
+  "type": "Section",
+  "number": "13.2",
+  "title": "Randomization Test to Compare Two Proportions",
+  "body": " Randomization Test to Compare Two Proportions  Text of section.  "
+},
+{
+  "id": "section-ch13-chi-square",
+  "level": "1",
+  "url": "section-ch13-chi-square.html",
+  "type": "Section",
+  "number": "13.3",
+  "title": "Chi-Square for Comparing Multiple Proportions",
+  "body": " Chi-Square for Comparing Multiple Proportions  Text of section.  "
+},
+{
+  "id": "section-ch14-ci-review",
+  "level": "1",
+  "url": "section-ch14-ci-review.html",
+  "type": "Section",
+  "number": "14.1",
+  "title": "Bootstrap and Confidence Intervals when Comparing Groups",
+  "body": " Bootstrap and Confidence Intervals when Comparing Groups  Text of section.  "
+},
+{
+  "id": "section-ch14-ci-compare-2mean",
+  "level": "1",
+  "url": "section-ch14-ci-compare-2mean.html",
+  "type": "Section",
+  "number": "14.2",
+  "title": "Difference between Two Population Means",
+  "body": " Difference between Two Population Means  Text of section.  "
+},
+{
+  "id": "section-ch14-ci-compare-2prop",
+  "level": "1",
+  "url": "section-ch14-ci-compare-2prop.html",
+  "type": "Section",
+  "number": "14.3",
+  "title": "Difference between Two Population Proportions",
+  "body": " Difference between Two Population Proportions  Text of section.  "
+},
+{
+  "id": "section-ch15-summarize-two-quant",
+  "level": "1",
+  "url": "section-ch15-summarize-two-quant.html",
+  "type": "Section",
+  "number": "15.1",
+  "title": "Summarizing Two Quantitative Variables",
+  "body": " Summarizing Two Quantitative Variables  Text of section.  "
+},
+{
+  "id": "section-ch15-regression-model",
+  "level": "1",
+  "url": "section-ch15-regression-model.html",
+  "type": "Section",
+  "number": "15.2",
+  "title": "Regression to Model Linear Relationships",
+  "body": " Regression to Model Linear Relationships  Text of section.  "
+},
+{
+  "id": "section-ch15-regression-inference",
+  "level": "1",
+  "url": "section-ch15-regression-inference.html",
+  "type": "Section",
+  "number": "15.3",
+  "title": "Hypothesis Test and Confidence Interval for Slope",
+  "body": " Hypothesis Test and Confidence Interval for Slope  Text of section.  "
+},
+{
+  "id": "section-ch15-mult-regression-intro",
+  "level": "1",
+  "url": "section-ch15-mult-regression-intro.html",
+  "type": "Section",
+  "number": "15.4",
+  "title": "Introduction to Multiple Regression",
+  "body": " Introduction to Multiple Regression  Text of section.  "
+},
+{
+  "id": "section-ch16-CLT-two-prop",
+  "level": "1",
+  "url": "section-ch16-CLT-two-prop.html",
+  "type": "Section",
+  "number": "16.1",
+  "title": "Inference for Comparing Proportions",
+  "body": " Inference for Comparing Proportions  Text of section.  "
+},
+{
+  "id": "section-ch16-CLT-two-means",
+  "level": "1",
+  "url": "section-ch16-CLT-two-means.html",
+  "type": "Section",
+  "number": "16.2",
+  "title": "Inference for Comparing Means",
+  "body": " Inference for Comparing Means  Text of section.  "
+},
+{
+  "id": "section-ch16-CLT-regression-slope",
+  "level": "1",
+  "url": "section-ch16-CLT-regression-slope.html",
+  "type": "Section",
+  "number": "16.3",
+  "title": "Inference for Regression",
+  "body": " Inference for Regression  Text of section.  "
+},
+{
+  "id": "section-ch17-name",
+  "level": "1",
+  "url": "section-ch17-name.html",
+  "type": "Section",
+  "number": "17.1",
+  "title": "Section Title",
+  "body": " Section Title  Text of section.  "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
